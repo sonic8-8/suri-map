0f139d95d513ca676aaf8e7101f3be82d53e67ff
김은서 <kkkks9203@naver.com>
2026-05-06 17:59:31 +0900

    [BE/Docs] docs: test 클래스 삭제

--- backend/src/main/java/com/surimap/config/MybatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.surimap.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.surimap.domain") // mapper interface는 domain 애그리거트 패키지 안에 둔다
-public class MybatisConfig {
-}
