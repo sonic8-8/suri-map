876779c448be1e4d9c12a56a5f19ca8f49677fc6
김은서 <kkkks9203@naver.com>
2026-05-06 14:26:33 +0900

    [BE] feat: 백엔드 실행 기반 및 마이그레이션 러너 구현 문서 삭제

--- backend/src/main/java/com/surimap/domain/event/EventOutboxMapper.java
+++
@@ -1,8 +0,0 @@
-package com.surimap.domain.event;
-
-import org.apache.ibatis.annotations.Mapper;
-
-// L2-B01 skeleton: method는 L2-T06 이후 task에서 추가
-@Mapper
-public interface EventOutboxMapper {
-}
