70b829af074170b6525193d14f172bd65e267fdb
이승환 <seaung13@gmail.com>
2026-05-07 10:29:46 +0900

    [BE] test(op): OP1 실패 fixture와 current OP 계약 보강 (S14P31C106-102)

--- backend/src/test/java/com/surimap/maparea/support/PostGisIntegrationTestSupport.java
+++ backend/src/test/java/com/surimap/maparea/support/PostGisIntegrationTestSupport.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -17,6 +18,7 @@ import org.testcontainers.utility.DockerImageName;
 // TODO 통합테스트를 위한 인프라 기반이 존재하지 않을 것으로 예상하여, 우선 mr 기본 테스트 범위에서 제외.
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Tag("integration")
 @Testcontainers(disabledWithoutDocker = true)
 public abstract class PostGisIntegrationTestSupport {
